4211de6db86248d270d3ffe02ff57f6b7962753e
yarwen0 <nhoojahm@gmail.com>
2025-03-03 15:40:19 -0600

    Initial commit - Wireflow

---
+++ app/_components/AppSidebar.tsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import Image from 'next/image'
+
+const items = [
+    {
+        title: "Home",
+        url: "#",
+        icon: Home,
+    },
+    {
+        title: "Inbox",
+        url: "#",
+        icon: Inbox,
+    },
+    {
+        title: "Calendar",
+        url: "#",
+        icon: Calendar,
+    },
+    {
+        title: "Search",
+        url: "#",
+        icon: Search,
+    },
+    {
+        title: "Settings",
+        url: "#",
+        icon: Settings,
+    },
+]
+
+export function AppSidebar() {
+    return (
+        <Sidebar>
+            <SidebarHeader>
+                <div className='p-4'>
+                    <Image src={'./logo.svg'} alt='logo' width={100} height={100}
+                        className='w-full h-full' />
+                    <h2 className='text-sm text-gray-400 text-center'>Build Awesome</h2>
+                </div>
+            </SidebarHeader>
+            <SidebarContent>
+                <SidebarGroup>
+
+                    <SidebarGroupContent>
+                        <SidebarMenu className='mt-5'>
+                            {items.map((item, index) => (
+                                // <SidebarMenuItem key={item.title} className='p-2'>
+                                //     <SidebarMenuButton asChild className=''>
+                                <a href={item.url} key={index} className='p-2 text-lg flex gap-2 items-center
+                                 hover:bg-gray-100 rounded-lg'>
+                                    <item.icon className='h-5 w-5' />
+                                    <span>{item.title}</span>
+                                </a>
+                                //     </SidebarMenuButton>
+                                // </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter>
+                <h2 className='p-2 text-gray-400 text-sm'>Copyright @Tubeguruji</h2>
+            </SidebarFooter>
+        </Sidebar>
+    )
+}
